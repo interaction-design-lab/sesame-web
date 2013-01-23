cdff2334ae11a618cebcb7ff9e006870fb887878
Phil Adams <phil@philadams.net>
2013-01-23 18:08:26 -0500

    basic line chart with time as x-axis

--- js/stress-sense-portal.js
+++ js/stress-sense-portal.js
@@ -1,29 +1,63 @@
-// chart
-var chart = timeSeriesChart()
-    .x(function(d) { return d.when; })
-    .y(function(d) { return d.stress; });
+// vars setup
+var w = 800,
+    h = 200,
+    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
 
-// load data and bind to chart
+var parseDate = d3.time.format.utc('%Y-%m-%d %H:%M:%S').parse;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.when); })
+    .y(function(d) { return y(d.stress); })
+    .interpolate('linear');
+
+var chart = d3.select('#chart-stress').append('svg')
+    .attr('width', w)
+    .attr('height', h)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
 d3.csv('./data/data.csv', function(data) {
-
-    // vars setup
-    var format = d3.time.format.utc('%Y-%m-%d %H:%M:%S'),
-        w = 815,
-        h = 500;
 
     // finesse data
     data.forEach(function(d, i) {
         d.stress = parseInt(d.stress);
         d.healthiness = parseInt(d.healthiness);
         d.utc = d.when;
-        d.when = format.parse(d.when);  // in local time
-        console.log(d);
+        d.when = parseDate(d.when);  // in local time
+        //console.log(d);
     });
 
-    d3.select('#chart-stress svg')
-        .datum(data)
-        .call(chart);
+    x.domain(d3.extent(data, function(d) { return d.when; }));
+    y.domain([0, 5]);
 
+    chart.append('g')
+        .attr('class', 'xAxis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+    chart.append('g')
+        .attr('class', 'yAxis')
+        .call(yAxis);
+
+    chart.append('path')
+        .data(data)
+        .attr('class', 'line')
+        .attr('d', line(data));
 
 });
 
